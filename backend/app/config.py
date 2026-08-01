3b291699b8500c0e1fffe3159c94ba9b86fe0107
mohsiniqubal <mohsiniqubal8@gmail.com>
2026-08-01 22:33:53 +0530

    Use DATABASE_URL for production

--- backend/app/config.py
+++ backend/app/config.py
@@ -3,12 +3,5 @@ import os
 
 load_dotenv()
 
-DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT")
-DB_NAME = os.getenv("DB_NAME")
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-
-DATABASE_URL = (
-    f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-)
+DATABASE_URL = os.getenv("DATABASE_URL")
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
